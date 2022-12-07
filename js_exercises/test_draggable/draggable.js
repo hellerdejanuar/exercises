fc83a23e2b1c325c96935342361f9b775392f93b
Mauricio Heller <hellerdejanuar@gmail.com>
2022-12-07 17:59:34 -0300

    Boilerplate

--- js_exercises/test_draggable/draggable.js
+++ js_exercises/test_draggable/draggable.js
@@ -1,14 +1,50 @@
 let audio = new Audio('resources/sounds/knock.mp3');
+let draggableElem = document.getElementById('box');
+let initialX = 0,
+    initialY = 0;
+let moveElement = false;
+let deviceType = null;
 
-
+//Events Object 
+let events = {
+    mouse: {
+        down: "mousedown",
+        move: "mousemove",
+        up: "mouseup",
+    },
+    touch: {
+        down: "touchstart",
+        move: "touchmove",
+        up: "touchend",
+    },
+};
 // handle mouse
 const mouse = {
     x: null,
     y: null,
-    radius: 150
+    radius: 150,
 }
 
 document.getElementById('box').addEventListener('click', function(e){
     console.log("bang");
     audio.play();
 })
+
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        deviceType = "touch";
+        return true;
+
+    } catch (e) {
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice();
+
+// Start (mouse down // touch start)
+draggableElem.addEventListener(events[deviceType].down, (e) => {
+    
+});
