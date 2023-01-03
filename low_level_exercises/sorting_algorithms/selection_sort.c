416166d88dd47eb800363d1acad842110536373b
Mauricio Heller <hellerdejanuar@gmail.com>
2023-01-03 14:54:28 -0300

    selection sort added

---
+++ low_level_exercises/sorting_algorithms/selection_sort.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * swap_elements: swaps two elements of an array
+ **/
+void swap_elements(int *array, size_t x, size_t y) {
+	size_t aux = array[x];
+
+	array[x] = array[y];
+	array[y] = aux; 
+} 
+
+/**
+ * selection_sort: Function for applying selection sort algo
+ * @array: array to sort
+ * @size: size of the array
+ **/
+void selection_sort(int *array, size_t size) {
+	size_t i, j;
+	for (i = 0; i < size; i ++) {
+		for (j = i; j < size; j++) {
+			if (array[i] <= array[j])
+				continue;
+			swap_elements(array, i, j);
+		}
+	}
+	return;
+}
